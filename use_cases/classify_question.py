d9101e5a7fd7275929d54aef14c195badc8e9789
Camilo Leonel Amadio <camilo.amadio@microchip.com>
2025-06-05 17:33:32 +0200

    more cleaning

--- use_cases/classify_question.py
+++ use_cases/classify_question.py
@@ -1,14 +1,5 @@
+from domain.classify_question import BaseQuestionClassifier
 from domain.document import Document, EmptyResponse, Documents
-from abc import ABC, abstractmethod
-
-class BaseQuestionClassifier(ABC):
-    """
-    Base class for question classifiers.
-    This class can be extended to implement different classification strategies.
-    """
-    @abstractmethod
-    def classify(self, question: str) -> Document| EmptyResponse:
-        raise NotImplementedError("This method should be overridden by subclasses.")
 
 class QuestionClassifier(BaseQuestionClassifier):
     """

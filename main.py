c0153f2558c7b46931dd8c2a517ea941e05e9ab4
Camilo Leonel Amadio <camilo.amadio@microchip.com>
2025-06-11 11:03:09 +0200

    added online match classifier in main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from rag_system.infrastructure.document_loader import OfflineDocumentLoader
 from rag_system.infrastructure.llm_client import LLMClient, OfflineLLMClient
 from rag_system.interfaces.gradio_ui import launch_gradio
 from rag_system.interfaces.strategies_classifier.exact_match_classifier import ExactMatchClassifier
+from rag_system.interfaces.strategies_classifier.llm_classifier import LLMClassifier
 from rag_system.use_cases.answer_generator import AnswerGenerator, OfflineAnswerGenerator
 from rag_system.use_cases.agent import Agent
 
@@ -14,7 +15,7 @@ def main(offline: bool):
 
     # This need to be updated when I have another document loader (e.g. from a vector store)
     offline_document_loader = OfflineDocumentLoader() if offline else OfflineDocumentLoader()
-    question_classifier = ExactMatchClassifier(document_loader=offline_document_loader)
+    question_classifier = ExactMatchClassifier(document_loader=offline_document_loader) if offline else LLMClassifier(offline_document_loader, llm)
 
     agent = Agent(question_classifier, answer_generator)
     launch_gradio(agent)

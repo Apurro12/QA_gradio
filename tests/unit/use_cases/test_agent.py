a6b726a087342abacc5e96a0c5b77188e3f2550c
Camilo Leonel Amadio <camilo.amadio@microchip.com>
2025-07-02 17:48:06 +0200

    the tests are working

--- tests/unit/use_cases/test_agent.py
+++ tests/unit/use_cases/test_agent.py
@@ -5,63 +5,54 @@ import pytest
 from rag_system.domain.agent import BaseAgent
 from rag_system.domain.conversation_manager import Message
 from rag_system.infrastructure.conversation_manager import InMemoryConversationManager
-from rag_system.infrastructure.document_loader import OfflineDocumentLoader
 from rag_system.use_cases.llm_client import OfflineLLMClient
-from rag_system.use_cases.tools.documents_retrival.strategies.exact_match_retrieval_strategy import ExactMatchRetrievalStrategy
 
 from rag_system.use_cases.agent import Agent
-from rag_system.use_cases.answer_generator import OfflineAnswerGenerator
-from langchain.openai import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
 ## TO DO
 ## ADD THE CASE WHERE THE LOADED GIVES NO DOCUMENTS
 @pytest.fixture
 def offline_agent():
-
     llm = OfflineLLMClient(ChatOpenAI())
-    offline_document_loader = OfflineDocumentLoader()
-    question_classifier = ExactMatchRetrievalStrategy(document_loader=offline_document_loader)
-    answer_generator = OfflineAnswerGenerator(llm=llm)
-    return Agent(question_classifier, answer_generator)
+    return Agent(llm, None)
 
 
 @pytest.fixture
 def offline_agent_with_conversation_manager():
-    llm = OfflineLLMClient()
-    offline_document_loader = OfflineDocumentLoader()
-    question_classifier = ExactMatchRetrievalStrategy(document_loader=offline_document_loader)
-    answer_generator = OfflineAnswerGenerator(llm=llm)
+    llm = OfflineLLMClient(ChatOpenAI())
     in_memory_conversation_manager = InMemoryConversationManager()
-    return Agent(question_classifier, answer_generator, in_memory_conversation_manager)
+    return Agent(llm, in_memory_conversation_manager)
 
 
 def test_offline_agent_find_response(offline_agent: BaseAgent):
     empty_history: list[Message] = []
-    response = offline_agent.respond_user_question("How can I get a refund?", empty_history)
+    message = "How can I get a refund?"
+    response = offline_agent.chat(message, empty_history)
     assert isinstance(response, str)
 
-    expected_offline_response = "[OFFLINE ANSWER GENERATOR] [OFFLINE LLM CALL]"
-
+    expected_offline_response = f"Last message is: '{message}'. Available tools: '[]'"
     assert expected_offline_response == response
+
 
 
 def test_offline_agent_not_find_response(offline_agent: BaseAgent):
     empty_history: list[Message] = []
-    response = offline_agent.respond_user_question("What is the capital of France?", empty_history)
+    message = "How can I get a refund?"
+    response = offline_agent.chat(message, empty_history)
     assert isinstance(response, str)
 
-    expected_offline_response = "No relevant information found to answer your question."
+    expected_offline_response = f"Last message is: '{message}'. Available tools: '[]'"
     assert expected_offline_response == response
 
 
 def test_chat_with_no_conversation_manager(offline_agent: BaseAgent):
     empty_history: list[Message] = []
     return_value = "some response"
-    with patch.object(offline_agent, "respond_user_question", return_value=return_value) as mock_respond:
+    with patch("langchain_openai.ChatOpenAI") as mock_chat_openai:
         message = "Test question?"
         result = offline_agent.chat(message, empty_history)
-        mock_respond.assert_called_once_with(message, empty_history)
-        assert result == return_value
+        mock_chat_openai.assert_not_called()
 
 
 def test_chat_with_conversation_manager_no_history_no_context(
@@ -70,97 +61,5 @@ def test_chat_with_conversation_manager_no_history_no_context(
     empty_history: list[Message] = []
     message = "First message"
     result = offline_agent_with_conversation_manager.chat(message, empty_history)
-    assert result == "[OFFLINE ANSWER GENERATOR] [OFFLINE LLM CALL]"
-
-
-def test_chat_with_conversation_manager(offline_agent_with_conversation_manager: Agent):
-    empty_history: list[Message] = []
-    first_message = "First message"
-    # Patch the generate method to spy on its call
-    with patch.object(
-        offline_agent_with_conversation_manager.answer_generator,
-        "generate",
-        return_value="[OFFLINE ANSWER GENERATOR] [OFFLINE LLM CALL]",
-    ) as mock_generate:
-        result = offline_agent_with_conversation_manager.chat(first_message, empty_history)
-        mock_generate.assert_called_once()
-        # You can check the exact arguments passed
-        called_args, _ = mock_generate.call_args
-        assert called_args[0] == first_message
-        assert called_args[1] == ""
-        # Optionally, check that full_context contains expected substrings
-        # assert "Relevant Information:" in called_args[1]
-        assert result == "[OFFLINE ANSWER GENERATOR] [OFFLINE LLM CALL]"
-
-
-def test_chat_with_conversation_manager_with_context(
-    offline_agent_with_conversation_manager: Agent,
-):
-    empty_history: list[Message] = []
-    first_message = "When will my package arrive?"
-    # Patch the generate method to spy on its call
-    with patch.object(
-        offline_agent_with_conversation_manager.answer_generator,
-        "generate",
-        return_value="[OFFLINE ANSWER GENERATOR] [OFFLINE LLM CALL]",
-    ) as mock_generate:
-        result = offline_agent_with_conversation_manager.chat(first_message, empty_history)
-        mock_generate.assert_called_once()
-        # You can check the exact arguments passed
-        called_args, _ = mock_generate.call_args
-        assert called_args[0] == first_message
-        assert called_args[1] == "Relevant Information:\nShipping takes 3-5 business days."
-        # Optionally, check that full_context contains expected substrings
-        # assert "Relevant Information:" in called_args[1]
-        assert result == "[OFFLINE ANSWER GENERATOR] [OFFLINE LLM CALL]"
-
-
-def test_chat_with_conversation_manager_with_history(
-    offline_agent_with_conversation_manager: Agent,
-):
-    history: list[Message] = [
-        Message(role="user", content="Message1"),
-        Message(role="assistant", content="Response1"),
-    ]
-    user_message = "Message2"
-    # Patch the generate method to spy on its call
-    with patch.object(
-        offline_agent_with_conversation_manager.answer_generator,
-        "generate",
-        return_value="[OFFLINE ANSWER GENERATOR] [OFFLINE LLM CALL]",
-    ) as mock_generate:
-        result = offline_agent_with_conversation_manager.chat(user_message, history)
-        mock_generate.assert_called_once()
-        # You can check the exact arguments passed
-        called_args, _ = mock_generate.call_args
-        assert called_args[0] == user_message
-        # This should be moved to a funcion please
-        assert called_args[1] == f"Conversation History:\n{history[0].role}: {history[0].content}\n{history[1].role}: {history[1].content}\n \n"
-        assert result == "[OFFLINE ANSWER GENERATOR] [OFFLINE LLM CALL]"
-
-
-def test_chat_with_conversation_manager_with_history_and_context(
-    offline_agent_with_conversation_manager: Agent,
-):
-    history: list[Message] = [
-        Message(role="user", content="Message1"),
-        Message(role="assistant", content="Response1"),
-    ]
-    user_message = "When will my package arrive?"
-    # Patch the generate method to spy on its call
-    with patch.object(
-        offline_agent_with_conversation_manager.answer_generator,
-        "generate",
-        return_value="[OFFLINE ANSWER GENERATOR] [OFFLINE LLM CALL]",
-    ) as mock_generate:
-        result = offline_agent_with_conversation_manager.chat(user_message, history)
-        mock_generate.assert_called_once()
-        # You can check the exact arguments passed
-        called_args, _ = mock_generate.call_args
-        assert called_args[0] == user_message
-        # This should be moved to a funcion please
-        assert (
-            called_args[1]
-            == f"Conversation History:\n{history[0].role}: {history[0].content}\n{history[1].role}: {history[1].content}\n \nRelevant Information:\nShipping takes 3-5 business days."
-        )
-        assert result == "[OFFLINE ANSWER GENERATOR] [OFFLINE LLM CALL]"
+    expected_offline_response = f"Last message is: '{message}'. Available tools: '[]'"
+    assert result == expected_offline_response
--- tests/unit/use_cases/test_answer_generator.py
+++ tests/unit/use_cases/test_answer_generator.py
@@ -2,27 +2,29 @@ from unittest.mock import MagicMock
 
 import pytest
 
+from rag_system.domain.conversation_manager import Message
 from rag_system.use_cases.llm_client import OfflineLLMClient
 from rag_system.use_cases.answer_generator import (
     AnswerGenerator,
     OfflineAnswerGenerator,
 )
 
+from langchain_openai import ChatOpenAI
 
 class TestOfflineAnswerGenerator:
     @pytest.fixture
     def offline_answer_generator(self):
-        llm = OfflineLLMClient()
+        llm = OfflineLLMClient(ChatOpenAI())
         answer_generator = OfflineAnswerGenerator(llm=llm)
         return answer_generator
 
     def test_generate(self, offline_answer_generator: OfflineAnswerGenerator):
         test_question = "This is a test question"
-        test_response = "This is a test context"
-        offline_response = offline_answer_generator.generate(test_question, test_response)
+        message = Message(role="user", content=test_question)
+        offline_response = offline_answer_generator.generate([message])
         assert isinstance(offline_response, str)
 
-        expected_offline_response = f"[OFFLINE ANSWER GENERATOR] {offline_answer_generator.llm.invoke('[OFFLINE LLM CALL]')}"
+        expected_offline_response = f"[OFFLINE ANSWER GENERATOR], last message: {message}"
         assert offline_response == expected_offline_response
 
 
@@ -39,9 +41,7 @@ class TestAnswerGenerator:
 
     def test_generate(self, answer_generator: AnswerGenerator, mock_llm: MagicMock):
         question = "What is AI?"
-        context = "AI stands for Artificial Intelligence."
-        result = answer_generator.generate(question, context)
-
-        expected_prompt = f"respond this question:\n{question}\n\nbased in this context: \n{context}\n"
-        mock_llm.invoke.assert_called_once_with(expected_prompt)
+        message = Message(role="user", content=question)
+        result = answer_generator.generate([message])
+        mock_llm.invoke.assert_called_once_with([message])
         assert result == "mocked llm response"
